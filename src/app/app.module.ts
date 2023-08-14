0651d75198e8723dbae0bc8d15efd202dd42bd86
David_Holliday <dhholliday@icloud.com>
2023-08-14 16:31:18 +0100

    Create the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShopingEditComponent } from './shopping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule
